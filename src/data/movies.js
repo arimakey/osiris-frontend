d734ab2369779ee4a8b854682eaea43b6ddcfb77
arima_key <127151745+arimakey@users.noreply.github.com>
2024-08-28 11:24:19 -0500

    refactor: estructura de carpetas actualizada

---
+++ src/data/movies.js
@@ -0,0 +1,48 @@
+const movies = [
+    {
+        "id": 1,
+        "img": "https://lumiere-a.akamaihd.net/v1/images/1_preview_alienromulus_latam_1sht_048_v2_simp_cmyk_c04c_74c20d70.jpeg?region=0%2C0%2C1240%2C1772",
+        "title": "Alien",
+        "duration": "1h 57m",
+        "trailer": "https://example.com/alien-trailer.mp4",
+        "url": "https://example.com/alien"
+    },
+    {
+        "id": 2,
+        "img": "https://i.pinimg.com/originals/87/4c/f9/874cf9a5f29f8b4aead67f6b85f442b5.jpg",
+        "title": "The Batman",
+        "duration": "2h 56m",
+        "trailer": "https://example.com/batman-trailer.mp4",
+        "url": "https://example.com/batman"
+    },
+    {
+        "id": 3,
+        "img": "https://i.ebayimg.com/images/g/cE4AAOSwdW9Z77tX/s-l1200.jpg",
+        "title": "Oldboy",
+        "duration": "2h 0m",
+        "trailer": "https://example.com/oldboy-trailer.mp4",
+        "url": "https://example.com/oldboy"
+    },
+    {
+        "id": 4,
+        "img": "https://img.ecartelera.com/noticias/fotos/38000/38080/1.jpg",
+        "title": "Get Out",
+        "duration": "1h 44m",
+        "trailer": "https://example.com/getout-trailer.mp4",
+        "url": "https://example.com/getout"
+    },
+    {
+        "id": 5,
+        "img": "https://pics.filmaffinity.com/Lady_Bird-546261513-large.jpg",
+        "title": "Lady Bird",
+        "duration": "1h 34m"
+    },
+    {
+        "id": 6,
+        "img": "https://pics.filmaffinity.com/Lady_Bird-546261513-large.jpg",
+        "title": "Lady Bird",
+        "duration": "1h 34m"
+    }
+]
+
+export default movies;
