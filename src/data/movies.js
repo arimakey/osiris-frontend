2391fced8cc035d4ec6c2c98fde37ac7b5fd1b0c
Jefferson Peña <jefferson152530@gmail.com>
2024-08-28 15:25:08 -0500

    feat: añadi el componente MoviesMarathon y MoviesItems

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,48 +1,20 @@
 const movies = [
     {
         "id": 1,
-        "img": "https://lumiere-a.akamaihd.net/v1/images/1_preview_alienromulus_latam_1sht_048_v2_simp_cmyk_c04c_74c20d70.jpeg?region=0%2C0%2C1240%2C1772",
-        "title": "Alien",
-        "duration": "1h 57m",
-        "trailer": "https://example.com/alien-trailer.mp4",
-        "url": "https://example.com/alien"
+        "img": "https://i.pinimg.com/564x/7a/48/ff/7a48ffdbc69167a6928ec3da3a9ac8b1.jpg",
+        "title": "El Joker",
+        "duration": "2h 25 min",
+        "trailer": "/resources/videos/trailerJoker.mp4",
+        "url": "https://example.com/joker"
     },
     {
         "id": 2,
-        "img": "https://i.pinimg.com/originals/87/4c/f9/874cf9a5f29f8b4aead67f6b85f442b5.jpg",
-        "title": "The Batman",
-        "duration": "2h 56m",
-        "trailer": "https://example.com/batman-trailer.mp4",
-        "url": "https://example.com/batman"
+        "img": "https://i.pinimg.com/564x/bf/63/c3/bf63c35faf47b36d4a12a8a9e8763932.jpg",
+        "title": "La Isla Siniestra",
+        "duration": "2h 18 min",
+        "trailer": "/resources/videos/islaSiniestraTrailer.mp4",
+        "url": "https://example.com/laislasiniestra"
     },
-    {
-        "id": 3,
-        "img": "https://i.ebayimg.com/images/g/cE4AAOSwdW9Z77tX/s-l1200.jpg",
-        "title": "Oldboy",
-        "duration": "2h 0m",
-        "trailer": "https://example.com/oldboy-trailer.mp4",
-        "url": "https://example.com/oldboy"
-    },
-    {
-        "id": 4,
-        "img": "https://img.ecartelera.com/noticias/fotos/38000/38080/1.jpg",
-        "title": "Get Out",
-        "duration": "1h 44m",
-        "trailer": "https://example.com/getout-trailer.mp4",
-        "url": "https://example.com/getout"
-    },
-    {
-        "id": 5,
-        "img": "https://pics.filmaffinity.com/Lady_Bird-546261513-large.jpg",
-        "title": "Lady Bird",
-        "duration": "1h 34m"
-    },
-    {
-        "id": 6,
-        "img": "https://pics.filmaffinity.com/Lady_Bird-546261513-large.jpg",
-        "title": "Lady Bird",
-        "duration": "1h 34m"
-    }
 ]
 
 export default movies;
