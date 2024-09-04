c66bf680e601f6e8acffdf3c7a4bcbb0288bbf08
arima_key <127151745+arimakey@users.noreply.github.com>
2024-09-03 23:44:05 -0500

    refactor: rename and move component to Faq page

---
+++ src/data/questions.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        question: "What's in the box?",
+        answer: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed suscipit praesentium fuga autem maiores quaerat molestias distinctio reiciendis quae iste soluta assumenda minus quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quaerat aliquid consectetur velit, reiciendis sed ipsa. Tenetur laborum repellat, officiis corrupti illo, culpa laboriosam provident voluptate laudantium, odit quis ipsa."
+    },
+    {
+        question: "¿Quién engañó a Roger Rabbit?",
+        answer: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed suscipit praesentium fuga autem maiores quaerat molestias distinctio reiciendis quae iste soluta assumenda minus quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quaerat aliquid consectetur velit, reiciendis sed ipsa. Tenetur laborum repellat, officiis corrupti illo, culpa laboriosam provident voluptate laudantium, odit quis ipsa."
+    },
+    {
+        question: "What's in the box?",
+        answer: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed suscipit praesentium fuga autem maiores quaerat molestias distinctio reiciendis quae iste soluta assumenda minus quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quaerat aliquid consectetur velit, reiciendis sed ipsa. Tenetur laborum repellat, officiis corrupti illo, culpa laboriosam provident voluptate laudantium, odit quis ipsa."
+    },
+    {
+        question: "What's in the box? ",
+        answer: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed suscipit praesentium fuga autem maiores quaerat molestias distinctio reiciendis quae iste soluta assumenda minus quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quaerat aliquid consectetur velit, reiciendis sed ipsa. Tenetur laborum repellat, officiis corrupti illo, culpa laboriosam provident voluptate laudantium, odit quis ipsa."
+    }
+]
+
+export default questions
