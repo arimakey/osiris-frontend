f34651aa69c874089474a847f4d0ae09813d54a6
arima_key <127151745+arimakey@users.noreply.github.com>
2024-09-10 16:21:50 -0500

    Reapply "Merge branch 'main' of https://github.com/arimakey/osiris-frontend"
    This reverts commit 7e0c51190e953b82b2701795e1ea90838d40084f.

---
+++ src/data/top_series.js
@@ -0,0 +1,94 @@
+const top_series = [
+    {
+        "id": 1,
+        "img": "https://bcdn.lanetaneta.com/wp-content/uploads/2022/07/1656883354_%C2%BFCuando-estara-la-temporada-6-de-Better-Call-Saul-en.jpg",
+        "title": "Better Call Saul",
+        "duration": "45m",
+        "description": "Seis años antes de conocer a Walter White, el cuestionable abogado Jimmy McGill asume el alias de Saul Goodman, mientras intenta labrarse un nombre propio en Albuquerque.",
+        "number": "1",
+        "url": "https://example.com/bettercallsaul"
+    },
+    {
+        "id": 2,
+        "img": "https://www.hola.com/horizon/landscape/93e86e7fd8eb-thewitcher-t.jpg",
+        "title": "The Witcher",
+        "duration": "47m",
+        "description": "El brujo Geralt, un cazador de monstruos, trata de encontrar su lugar en un mundo en el que las personas suelen ser más malvadas que las bestias.",
+        "number": "2",
+        "url": "https://example.com/thewitcher"
+    },
+    {
+        "id": 3,
+        "img": "https://fotografias.larazon.es/clipping/cmsimages02/2019/08/12/E9550A6F-902F-4FA3-874B-EAFFAD4C36A6/98.jpg?crop=2000,1125,x0,y0&width=1900&height=1069&optimize=low&format=webply",
+        "title": "Game of Thrones",
+        "duration": "50m",
+        "description": "Dos familias poderosas, reyes y reinas, caballeros y renegados, hombres falsos y honestos, haciendo parte de un juego mortal por el control de los Siete Reinados de Westeros y por sentarse en el trono más alto. ",
+        "number": "3",
+        "url": "https://example.com/gameofthrones"
+    },
+    {
+        "id": 4,
+        "img": "https://pictures.betaseries.com/banners/episodes/17461/304bcf9b32a27a3b01b5c93cb6fc95c4.jpg",
+        "title": "Cobra Kai",
+        "duration": "48m",
+        "description": "Treinta y cuatro años después de los eventos del torneo de karate All Valley de 1984, Johnny Lawrence busca la redención al abrir un dojo Cobra Kai, reavivando su rivalidad con un exitoso Daniel LaRusso.",
+        "number": "4",
+        "url": "https://example.com/cobrakai"
+    },
+    {
+        "id": 5,
+        "img": "https://www.cined.com/content/uploads/2021/04/w1900_h1155_x991_y602_Omar_Sy_in_Lupin_Netflix-97f8351d7eafaad1.jpg",
+        "title": "Lupin",
+        "duration": "53m",
+        "description": "Basada en la historia de Arsène Lupin, el ladrón Assane Diop se propone vengar a su padre de las injusticias sufridas por parte de una familia adinerada.",
+        "number": "5",
+        "url": "https://example.com/readnotice"
+    },
+    {
+        "id": 6,
+        "img": "https://www.tomsguide.fr/content/uploads/sites/2/2023/11/Stranger-Things-saison-5.png",
+        "title": "Stranger Things",
+        "duration": "40m",
+        "description": "Cuando un niño desaparece, sus amigos, la familia y la policía se ven envueltos en una serie de eventos misteriosos al tratar de encontrarlo. Su ausencia coincide con el avistamiento de una criatura terrorífica y la aparición de una extraña niña.",
+        "number": "6",
+        "url": "https://example.com/strangerthings"
+    },
+    {
+        "id": 7,
+        "img": "https://media.revistagq.com/photos/637debfcefd28422764c6b26/4:3/w_960,h_720,c_limit/elite.jpeg",
+        "title": "Élite",
+        "duration": "45m",
+        "description": "Las Encinas es el colegio más exclusivo de España, el lugar donde estudian los hijos de la élite y donde acaban de ser admitidos tres jóvenes de clase baja, procedentes de un colegio público en ruinas.",
+        "number": "7",
+        "url": "https://example.com/elite"
+    },
+    {
+        "id": 8,
+        "img": "https://media.revistagq.com/photos/6135f5bf3bc2dbd6357b63a9/16:9/w_2560%2Cc_limit/la-casa-de-papel.jpg",
+        "title": "La casa de papel",
+        "duration": "50m",
+        "description": "Una banda organizada de ladrones se propone cometer el atraco del siglo en la Fábrica Nacional de Moneda y Timbre. Cinco meses de preparación quedarán reducidos a once días para poder llevar a cabo con éxito el gran golpe.",
+        "number": "8",
+        "url": "https://example.com/lacasadepapel"
+    },
+    {
+        "id": 9,
+        "img": "https://static01.nyt.com/images/2021/10/13/arts/11squid-notebook-esp-1/11squid-notebook-videoSixteenByNineJumbo1600.jpg",
+        "title": "El juego del calamar",
+        "duration": "53m",
+        "description": "Cientos de personas con dificultades económicas aceptan una extraña invitación a un juego de supervivencia. Les espera un premio millonario, pero hay mucho en juego.",
+        "number": "9",
+        "url": "https://example.com/juego del calamar"
+    },
+    {
+        "id": 10,
+        "img": "https://indiehoy.com/wp-content/uploads/2022/08/woo-una-abogada.jpg",
+        "title": "Woo, una abogada extraordinaria",
+        "duration": "40m",
+        "description": "Woo Young Woo es una joven abogada con síndrome de Asperger. Ella presume de tener un alto coeficiente intelectual, una memoria impresionante y un proceso de pensamiento muy creativo, pero tiene dificultades con las interacciones cotidianas.",
+        "number": "10",
+        "url": "https://example.com/Woo"
+    },
+]
+
+export default top_series;
