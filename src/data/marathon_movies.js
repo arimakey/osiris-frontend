7674b764bd101e170964d443272888c1e496bbc2
Jefferson Peña <jefferson152530@gmail.com>
2024-09-13 04:38:36 -0500

    feat: cree el componente MarathonItem y MarathonMovies

---
+++ src/data/marathon_movies.js
@@ -0,0 +1,20 @@
+const marathon_movies = [
+    {
+        "id": 1,
+        "img": "https://i.pinimg.com/564x/7a/48/ff/7a48ffdbc69167a6928ec3da3a9ac8b1.jpg",
+        "title": "El Joker",
+        "duration": "2h 25 min",
+        "gift": "https://i.pinimg.com/originals/84/12/75/841275bb16a13342c4478b803e836b2a.gif",
+        "url": "https://example.com/joker"
+    },
+    {
+        "id": 2,
+        "img": "https://i.pinimg.com/564x/bf/63/c3/bf63c35faf47b36d4a12a8a9e8763932.jpg",
+        "title": "La Isla Siniestra",
+        "duration": "2h 18 min",
+        "gift": "https://i.pinimg.com/originals/d5/a9/6d/d5a96df5f82626bda14ea5933a9bc4de.gif",
+        "url": "https://example.com/laislasiniestra"
+    },
+]
+
+export default marathon_movies;
