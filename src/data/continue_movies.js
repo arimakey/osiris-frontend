31b7d5fb08cec7f5f3bcedd54f140568dc5d3429
Jefferson Peña <jefferson152530@gmail.com>
2024-09-13 05:05:45 -0500

    feat: añadi los componentes ContinueItems y MovieContinue

---
+++ src/data/continue_movies.js
@@ -0,0 +1,44 @@
+const continue_movies = [
+    {
+        "id": 1,
+        "img": "https://m.media-amazon.com/images/S/pv-target-images/acc496c5bb7ee0783b384dc11e6d2a429813f88e39a3b0411f0ac82f9be79e6c.jpg",
+        "title": "El Planeta de los Simios",
+        "duration": "1h 45 min",
+        "time": "0h 48 min",
+        "url": "https://example.com/planetadelosimios"
+    },
+    {
+        "id": 2,
+        "img": "https://www.mubis.es/media/articles/1276/12339/detalles-del-blu-ray-de-cuentos-de-terramar-l.jpg",
+        "title": "Cuentos de Terramar",
+        "duration": "1h 55 min",
+        "time": "1h 00 min",
+        "url": "https://example.com/cuentosdetarramar"
+    },
+    {
+        "id": 3,
+        "img": "https://www.otroscines.com/images/fotos/habia-una-vez-en-hollywood-tarantino-critica-655.jpg",
+        "title": "Había una vez en HollyWood",
+        "duration": "2h 40 min",
+        "time": "2h 03 min",
+        "url": "https://example.com/habiaunavezenholliwood"
+    },
+    {
+        "id": 4,
+        "img": "https://images.squarespace-cdn.com/content/v1/56e834ebd51cd466c99178e5/e689cea2-53f3-420b-a205-f84bac2194fd/ElClupdelaPelea_02.jpg",
+        "title": "El Club de la Pelea",
+        "duration": "2h 19 min",
+        "time": "1h 18 min",
+        "url": "https://example.com/elclubdelapelea"
+    },
+    // {
+    //     "id": 5,
+    //     "img": "https://m.media-amazon.com/images/M/MV5BY2JhNzliZDQtOGYxNS00ZWY3LWFlZDAtOTkyMDQ3ZWJiMjAxXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    //     "title": "Terminator 2",
+    //     "duration": "2h 17 min",
+    //     "time": "1h 40 min",
+    //     "url": "https://example.com/elclubdelapelea"
+    // },
+];
+
+export default continue_movies;
